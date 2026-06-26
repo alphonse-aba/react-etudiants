871bc1b2a30bb55200293eeb24e31bdb5608b2b6
alphonse-aba <alphonseabafoum357@gmail.com>
2026-06-26 20:23:54 +0000

    full Stack React + Django complet

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,62 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-
-  // Les useState
-  const [etudiants, setEtudiants] = useState([
-    { id: 1, nom: "Alphonse", ville: "Lomé" },
-    { id: 2, nom: "Kossi", ville: "Kara" },
-  ])
+  const [etudiants, setEtudiants] = useState([])
   const [nom, setNom] = useState("")
-  const [ville, setVille] = useState("")
 
-  // Ajouter un étudiant
-  function ajouterEtudiant() {
-    if (nom === "" || ville === "") {
-      alert("Veuillez remplir tous les champs !")
-      return
-    }
-    let nouvelEtudiant = {
-      id: etudiants.length + 1,
-      nom: nom,
-      ville: ville
-    }
-    setEtudiants([...etudiants, nouvelEtudiant])
-    setNom("")
-    setVille("")
+  useEffect(() => {
+    chargerEtudiants()
+  }, [])
+
+  async function chargerEtudiants() {
+    let reponse = await fetch("http://127.0.0.1:8000/etudiants/api/")
+    let data = await reponse.json()
+    setEtudiants(data.etudiants)
   }
 
-  // Supprimer un étudiant
-  function supprimerEtudiant(id) {
-    setEtudiants(etudiants.filter((e) => e.id !== id))
+  async function ajouterEtudiant() {
+    if (nom === "") {
+      alert("Veuillez entrer un nom !")
+      return
+    }
+    await fetch("http://127.0.0.1:8000/etudiants/api/ajouter/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ nom: nom })
+    })
+    setNom("")
+    chargerEtudiants()
+  }
+
+  async function supprimerEtudiant(id) {
+    await fetch("http://127.0.0.1:8000/etudiants/api/supprimer/" + id + "/", {
+      method: "DELETE",
+    })
+    chargerEtudiants()
   }
 
   return (
     <div>
-      <h1>🎓 Gestionnaire d'étudiants</h1>
+      <h1>🎓 Étudiants depuis Django</h1>
 
-      {/* Formulaire */}
       <input
         type="text"
-        placeholder="Nom..."
+        placeholder="Nom de l'étudiant..."
         value={nom}
         onChange={(e) => setNom(e.target.value)}
       />
-      <input
-        type="text"
-        placeholder="Ville..."
-        value={ville}
-        onChange={(e) => setVille(e.target.value)}
-      />
       <button onClick={ajouterEtudiant}>Ajouter</button>
 
-      {/* Liste */}
       <ul>
-        {etudiants.map((etudiant) => (
-          <li key={etudiant.id}>
-            {etudiant.nom} — {etudiant.ville}
-            <button onClick={() => supprimerEtudiant(etudiant.id)}>
+        {etudiants.map((e) => (
+          <li key={e.id}>
+            {e.nom}
+            <button onClick={() => supprimerEtudiant(e.id)}>
               Supprimer
             </button>
           </li>
         ))}
       </ul>
-
     </div>
   )
 }
